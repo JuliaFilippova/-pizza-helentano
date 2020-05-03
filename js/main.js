ee4456f7acfd35d55f6da4744a0b7eb4947788cd
Julia Filippova <mega.yulia1996@mail.ru>
2020-05-04 00:32:03 +0300

    +

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    $('.header-banner-slick').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        responsive: [{
+            breakpoint: 424,
+            settings: {}
+        }]
+    });
+
+    $('.product-selection-slider').slick({
+        dots: false,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        arrows: true,
+        responsive: [{
+            breakpoint: 424,
+            settings: {}
+        }]
+    });
+});
